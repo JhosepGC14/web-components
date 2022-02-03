54b4836ca37317cef51e7e073b4b0558b6078333
JhosepGC14 <jhosepgc14@gmail.com>
2022-02-03 12:55:32 -0500

    se agrego input password con vanilla js

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const tpl = document.createElement("template");
    3.1) agregar una imagen que represente el resultado del imc 
    4) opcional, tener la posibilidad de crear elementos dinamicamente que se puedan utilizar  ejemplo
       input con descripcion , poner boton mostrar y muestra todo en texto plano dentro de un div
-       
-
    */
 
 tpl.innerHTML = `
@@ -58,22 +56,14 @@ class NeoInput extends HTMLElement {
     //EJERCICIO N°1
     let inputCustom = this._shadowRoot.getElementById("inputCustom");
     let divInputCustom = this._shadowRoot.getElementById("resultInputCustom");
-    inputCustom.addEventListener("change", (e) => {
-      let lastValue = inputCustom.value;
-      inputCustom.value = "";
-      for (let index = 0; index < lastValue.length; index++) {
-        inputCustom.value += "|";
-      }
-      divInputCustom.innerHTML = lastValue;
+
+    inputCustom.addEventListener("keyup", (e) => {
+      console.log("inputCustom.value : ", inputCustom.value);
+      let lastValue = inputCustom.value.substr(inputCustom.value.length - 1);
+      console.log("lastValue : ", lastValue);
+      divInputCustom.textContent += lastValue;
+      inputCustom.value = inputCustom.value.replace(/[A-Za-z10-9]/g, "|");
     });
-    // inputCustom.addEventListener("keypress", (e) => {
-    //   let lastValue = inputCustom.value;
-    //   inputCustom.value = "";
-    //   for (let index = 0; index < lastValue.length; index++) {
-    //     inputCustom.value += "|";
-    //   }
-    //   divInputCustom.innerHTML = lastValue;
-    // });
 
     //EJERCICIO N°2
     let weightValue = this._shadowRoot.getElementById("inputPeso");
@@ -82,22 +72,68 @@ class NeoInput extends HTMLElement {
     let resultIMC = this._shadowRoot.getElementById("resultIMC");
     let imageResult = this._shadowRoot.getElementById("imageResult");
 
-    // "http://endocrino.cat/IMAGES_26/dieta-delgadez.jpg" bajo peso
-
     const calculateImc = () => {
       if (weightValue.value && heightValue.value) {
         let newHeight = Math.pow(Number(heightValue.value), 2);
-        console.log("newHeight...", newHeight);
-        console.log("newHeight...", typeof newHeight);
         let result = Number(weightValue.value) / newHeight;
-        console.log("result...", result);
-        console.log("result...", typeof result);
-        resultIMC.innerHTML = result;
-        imageResult.setAttribute("src", getImageAsResult(result));
+        resultIMC.innerHTML = `
+        Resultado : ${result.toFixed(2)}
+        
+        Tienes : ${getImageAsResult(result).text}`;
+        imageResult.setAttribute("src", getImageAsResult(result).image);
       }
     };
 
-    const getImageAsResult = (resultImc) => {};
+    const getImageAsResult = (resultImc) => {
+      if (resultImc < 15) {
+        return {
+          text: "Delgadez muy severa",
+          image:
+            "https://doblementesaludable.com/wp-content/uploads/2019/01/Delgadez-extrema-7.jpg",
+        };
+      } else if (resultImc > 15 && resultImc < 15.9) {
+        return {
+          text: "Delgadez severa",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw1TJmBXMp_k04hch_yrE1NTwppovhLteRhA&usqp=CAU",
+        };
+      } else if (resultImc > 16 && resultImc < 18.4) {
+        return {
+          text: "Delgadez",
+          image: "http://endocrino.cat/IMAGES_26/dieta-delgadez.jpg",
+        };
+      } else if (resultImc > 18.5 && resultImc < 24.9) {
+        return {
+          text: "Peso saludable",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn2WKFdLtBIOaGf_F2RwU0qk4ZaaZU7gRKZw&usqp=CAU",
+        };
+      } else if (resultImc > 25 && resultImc < 29.9) {
+        return {
+          text: "Sobrepeso",
+          image:
+            "https://www.portafolio.co/files/article_multimedia/uploads/2016/02/07/56b789fd73d81.jpeg",
+        };
+      } else if (resultImc > 30 && resultImc < 34.9) {
+        return {
+          text: "Obesidad moderada",
+          image:
+            "https://918230.smushcdn.com/2283449/wp-content/uploads/2020/05/ecoticias.jpg?lossy=1&strip=1&webp=1",
+        };
+      } else if (resultImc > 35 && resultImc < 39.9) {
+        return {
+          text: "Obesidad severa",
+          image:
+            "https://www.elindependiente.com/wp-content/uploads/2020/06/20200108285db5a42486a2e4728937cb6dacfdbb.jpg",
+        };
+      } else if (resultImc > 40) {
+        return {
+          text: "Obesidad muy severa",
+          image:
+            "https://www.iidenut.org/instituto/wp-content/uploads/2021/03/60-obesidad-y-covid.jpg",
+        };
+      }
+    };
 
     btnCalculate.addEventListener("click", calculateImc);
   }
